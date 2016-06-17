668e2a63d5e10748e242520bd6bde15043e8448d
fcaimi <fedeoasi@gmail.com>
2016-06-16 21:57:42 -0500

    Remove documentation that is not needed because it is definitely going to get out of date.

--- app/filters/Filters.scala
+++ app/filters/Filters.scala
@@ -4,15 +4,6 @@ import javax.inject._
 
 import play.api.http.HttpFilters
 
-/**
- * This class configures filters that run on every request. This
- * class is queried by Play to get a list of filters.
- *
- * Play will automatically use filters from any class called
- * `filters.Filters` that is placed the root package. You can load filters
- * from a different class by adding a `play.http.filters` setting to
- * the `application.conf` configuration file.
- */
 @Singleton
 class Filters @Inject() (loggingFilter: LoggingFilter) extends HttpFilters {
   override val filters = Seq(loggingFilter)
--- app/modules/Module.scala
+++ app/modules/Module.scala
@@ -1,29 +1,15 @@
 package modules
 
-import java.time.Clock
-
 import com.google.inject.AbstractModule
 import persistence.SQLiteDatabaseInitializer
 import persistence.auth.{AuthPersistenceService, AuthPersistenceServiceImpl}
-import services.ApplicationTimer
 
 import scala.slick.driver.JdbcDriver.simple._
 
-/**
- * This class is a Guice module that tells Guice how to bind several
- * different types. This Guice module is created when the Play
- * application starts.
-  *
-  * Play will automatically use any class called `modules.Module` that is in
- * the root package. You can create modules in other locations by
- * adding `play.modules.enabled` settings to the `application.conf`
- * configuration file.
- */
 class Module extends AbstractModule {
 
   override def configure() = {
     val database = SQLiteDatabaseInitializer.database("courses")
-
     bind(classOf[Database]).toInstance(database)
     bind(classOf[AuthPersistenceService]).toInstance(new AuthPersistenceServiceImpl(database))
   }

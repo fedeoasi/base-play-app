9cee5153f0123c2abd3cb19c766c3f9e65baa804
fcaimi <fedeoasi@gmail.com>
2016-06-16 21:53:29 -0500

    Make index page protected by authentication.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,15 @@
 package controllers
 
 import javax.inject._
-import play.api._
-import play.api.mvc._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+import securesocial.MyRuntimeEnvironment
+import securesocial.core.SecureSocial
+
 @Singleton
-class HomeController @Inject() extends Controller {
+class HomeController @Inject() (override implicit val env: MyRuntimeEnvironment)
+  extends SecureSocial {
 
-  /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index = Action {
+  def index = SecuredAction {
     Ok(views.html.index("Your new application is ready."))
   }
-
 }
